474560c144bcc4da095fcb904481ea37f15c0f52
benoit <benoitvicaireformation@gmail.com>
2026-05-04 15:06:00 +0200

    fix: todolist

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,30 @@
+function ListElement({index , content, date}){
 
-function List(){
+	return (
+		<li index={index}>{content} - {date}</li>
+	)
+
+}
+
+function List({date}){
 
     return(
     <ul>
-      <li>Metro</li>
-      <li>Boulo</li>
-      <li>Dodo</li>
+		<ListElement
+			index={1}
+			content={"metro"}
+			date={"04/05/2026"}
+		/>
+		<ListElement
+			index={2}
+			content={"boulo"}
+			date={"05/05/2026"}
+		/>
+		<ListElement
+			index={3}
+			content={"dodo"}
+			date={"06/05/2026"}
+		/>
     </ul>
     );
 }
--- src/components/ToDoCard.jsx
+++ src/components/ToDoCard.jsx
@@ -1,12 +1,12 @@
 import List from './List';
 
 function Card(){
-    const DATE= Date()
+    const DATE= new Date().toLocaleDateString('fr-FR');
     return (
         <article>
             <h1>To Do List</h1>
             <h2>{DATE}</h2>
-            <List/>
+            <List />
 
         </article>
     );
